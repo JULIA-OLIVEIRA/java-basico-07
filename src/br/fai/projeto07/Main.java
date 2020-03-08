014a057cb4e3c614ed1998294e115d2f3f5f8ed1
unknown <julia.carvalho.oliveira33@gmail.com>
2020-03-08 19:56:32 -0300

    segundo alteracao

--- src/br/fai/projeto07/Main.java
+++ src/br/fai/projeto07/Main.java
@@ -1,5 +1,7 @@
 package br.fai.projeto07;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,7 +13,18 @@ public class Main {
 	
 	private void start() {
 		
+		System.out.println("digite um valor: ");
+		
+		Scanner scanner = new Scanner(System.in);
+
+		int valor1 = scanner.nextInt();
+		int valor2 = scanner.nextInt();
+		
+		int soma = valor1 + valor2;
+		
+		System.out.println("valor da soma: " + soma);
 		
 	}
+	
 
 }
